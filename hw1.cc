e006f62628f04899e46ade282672618241dcd75d
K-mach <chentenghung@gmail.com>
2020-03-18 22:55:25 +0800

    Documented the algorithm and data structures.

--- hw1.cc
+++ hw1.cc
@@ -1,7 +1,49 @@
+/*
+ * ALGO:
+ * Q <- init_state
+ * visited.Add(init_state.position)
+ * while Q is not empty:
+ *     curr_state <- Q.pop()
+ *     for [key, dx, dy] in 4 directions:
+ *         new_state <- computeState(curr_state, dx, dy)
+ *         if isSolved(new_state):
+ *             return moves.Get(curr_state) + key
+ *         if !visited[new_state.bp_pos] && testState(new_state):
+ *             Q.push(new_state)
+ *             visited.Add(new_state.position)
+ *             moves.Add(curr_state, new_state, key)
+ *
+ *
+ * DS:
+ * State:
+ *     game_map: vector<strings>
+ *     boxes & player position (position): vector<pair<int, int>>
+ *     hash_key: A string converted from `position`.
+ *
+ * visited: An unordered_set of strings.
+ *
+ * moves: A trie.
+ */
+
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <string>
+#include <unordered_set>
+#include <utility>
 #include <vector>
+
+class State {
+public:
+    State() {}
+    State(std::vector<std::string> game_map)
+    {
+    }
+
+    std::vector<std::string> game_map;
+    std::vector<std::pair<int, int>> position;
+    std::string hash_key;
+};
 
 class Direction {
 public:
@@ -21,12 +63,6 @@ int main(int argc, char* argv[])
     std::string line;
     std::ifstream testcase(argv[1]);
     std::vector<std::string> game_map;
-    std::vector<Direction> dir = {
-        { 'W', -1, 0 },
-        { 'A', 0, -1 },
-        { 'S', 1, 0 },
-        { 'D', 0, 1 }
-    };
 
     if (testcase.is_open()) {
         while (std::getline(testcase, line)) {
@@ -36,11 +72,14 @@ int main(int argc, char* argv[])
     } else
         std::cerr << "Unable to open file.\n";
 
-#ifdef DEBUG
-    for (const auto& [key, dx, dy] : dir) {
-        std::cerr << key << ' ' << dx << ' ' << dy << '\n';
-    }
-#endif
+    std::vector<Direction> dir = {
+        { 'W', -1, 0 },
+        { 'A', 0, -1 },
+        { 'S', 1, 0 },
+        { 'D', 0, 1 }
+    };
+    std::queue<State> Q;
+    std::unordered_set<std::string> visited;
 
     return 0;
 }
