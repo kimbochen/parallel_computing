abe874615e9a395e5e05a1c7e1119fe4be03a769
K-mach <chentenghung@gmail.com>
2020-03-22 19:21:59 +0800

    Rewrite version. Optimized with Boost library.

--- hw1.cc
+++ hw1.cc
@@ -1,384 +1,299 @@
-#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <boost/unordered_set.hpp>
 #include <fstream>
 #include <iostream>
+#include <omp.h>
 #include <queue>
 #include <sstream>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
+#include <tuple>
 #include <vector>
 
-using std::vector;
-using std::string;
-using std::queue;
+using GameMap = std::vector<std::vector<char>>;
+using State = std::tuple<GameMap, int, int, std::string>;
 
-class Coord {
+class Game {
 public:
-    Coord() {}
-
-    Coord(int x_, int y_)
+    Game(char* &filename)
     {
-        x = x_;
-        y = y_;
-    }
+        // Read file
+        std::ifstream inFile;
+        std::stringstream ss;
 
-    friend Coord operator + (const Coord &a, const Coord &b)
-    {
-        return Coord(a.x + b.x, a.y + b.y);
-    }
+        inFile.open(filename);
+        ss << inFile.rdbuf();
+        boost::split(initField, ss.str(), boost::is_any_of("\n"));
 
-    friend Coord operator * (int k, const Coord &a)
-    {
-        return Coord(k * a.x, k * a.y);
-    }
+        // Find player position
+        int n = initField.size() - 1;
+        int m = initField[0].size() - 1;
 
-    friend bool operator == (const Coord &a, const Coord &b)
-    {
-        return (a.x == b.x && a.y == b.y);
-    }
-#ifdef DEBUG
-    void debug() const
-    {
-        std::cout << '(' << x << ',' << y << ')';
-    }
-#endif
-    int x, y;
-};
+        #pragma omp parallel default(none) shared(initField, n, m, playerPosX, playerPosY)
+        {
+            int i, j;
 
-struct Move {
-    Move(char key_, int x, int y)
-    {
-        key = key_;
-        dir = Coord(x, y);
-    }
-
-    char key;
-    Coord dir;
-};
-
-class Node {
-public:
-    Node(Node* prevNode_ = nullptr, char key_ = '\0')
-    {
-        prevNode = prevNode_;
-        key = key_;
-    }
-
-    Node *prevNode;
-    char key;
-};
-
-class State {
-public:
-    State() {}
-
-    State (vector<string> &initGameMap)
-    {
-        gameMap = initGameMap;
-
-        for (int i = 1; i < initGameMap.size() - 1; i++) {
-            for (int j = 1; j < initGameMap[0].size() - 1; j++) {
-                char c = initGameMap[i][j];
-
-                if (c == 'x' || c == 'X')
-                    boxPos.emplace_back(i, j);
-                else if (c == 'o' || c == 'O')
-                    playerPos = Coord(i, j);
-            }
-        }
-
-        keyNode = new Node();
-
-        computeHashKey();
-    }
-
-    State operator + (const Move &m) const
-    {
-        State state;
-        Coord movedBoxPos = playerPos + 2 * m.dir;
-
-        state.gameMap = gameMap;
-        state.boxPos = boxPos;
-        state.playerPos = playerPos + m.dir;
-
-        char &prevPlayerTile = state.gameMap[playerPos.x][playerPos.y];
-        char &playerTile = state.gameMap[state.playerPos.x][state.playerPos.y];
-        char &boxTile = state.gameMap[movedBoxPos.x][movedBoxPos.y];
-
-        prevPlayerTile = (prevPlayerTile == 'o') ? ' ' : '.';
-
-        if (playerTile == ' ')
-            playerTile = 'o';
-        else if (playerTile == '.')
-            playerTile = 'O';
-        else {
-            playerTile = (playerTile == 'x') ? 'o' : 'O';
-            boxTile = (boxTile == ' ') ? 'x' : 'X';
-
-            for (auto& bp : state.boxPos) {
-                if (bp == state.playerPos) {
-                    bp = bp + m.dir;
-                    break;
+            #pragma omp for collapse(2) private(i, j)
+            for (i = 1; i < n; i++) {
+                for (j = 1; j < m; j++) {
+                    if (initField[i][j] == 'o' || initField[i][j] == 'O') {
+                        #pragma omp critical
+                        {
+                            playerPosX = i;
+                            playerPosY = j;
+                        }
+                    }
                 }
             }
         }
 
-        state.keyNode = new Node(keyNode, m.key);
-        state.computeHashKey();
+        // Initialize directions
+        direction = {
+            {'W', -1,  0},
+            {'A',  0, -1},
+            {'S',  1,  0},
+            {'D',  0,  1}
+        };
 
-        return state;
+        // Initialize answer string
+        ans = "Not Solved!\n";
     }
 
-    void computeHashKey()
+    bool isValidStep(const GameMap &field, int x, int y, int dx, int dy)
     {
-        std::ostringstream os;
+        char C2 = field[x + dx][y + dy];
 
-        os << playerPos.x << playerPos.y;
-        for (const auto& c : boxPos) {
-            os << c.x << c.y;
+        if (C2 == ' ' || C2 == '.') {
+            return true;
         }
-        hashKey = os.str();
-
+        else if (C2 == 'x' || C2 == 'X') {
+            char C3 = field[x + 2 * dx][y + 2 * dy];
+            return (C3 == ' ' || C3 == '.');
+        }
+        else return false;
     }
+
+    void makeStep(GameMap &field, int x, int y, int dx, int dy)
+    {
+        field[x][y] = (field[x][y] == 'o') ? ' ' : '.';
+
+        char &C2 = field[x + dx][y + dy];
+
+        if (C2 == ' ') {
+            C2 = 'o';
+        }
+        else if (C2 == '.') {
+            C2 = 'O';
+        }
+        else {
+            char &C3 = field[x + 2 * dx][y + 2 * dy];
+
+            C3 = (C3 == ' ') ? 'x' : 'X';
+            C2 = (C2 == 'x') ? 'o' : 'O';
+        }
+    }
+
+    bool isSolution(const GameMap &field)
+    {
+        int n = field.size() - 1;
+        int m = field[0].size() - 1;
+        bool isSol = true;
+
+        #pragma omp parallel default(none) shared(n, m, isSol, field)
+        {
+            int i, j;
+            char c;
+
+            #pragma omp for private(i, j, c) reduction(&& : isSol)
+            for (i = 1; i < n; i++) {
+                for (j = 1; j < m; j++) {
+                    c = field[i][j];
+                    isSol = isSol && (c != 'O' && c != 'x' && c != '.');
+                }
+            }
+        }
+
+        return isSol;
+    }
+
+    bool isDeadlock(const GameMap &field, int x, int y)
+    {
+        int n = field.size() - 1;
+        int m = field[0].size() - 1;
+
+        // A box is in a corner:
+        // ##~~ -> T
+        // ~##~ -> T
+        // ~~## -> T
+        // #~~# -> T
+        bool isCorner = false;
+
+        #pragma omp parallel default(none) shared(n, m, field, isCorner)
+        {
+            int i, j, k;
+            int dx1, dy1, dx2, dy2;
+            char adj1, adj2;
+
+            #pragma omp for private(i, j, k) reduction(|| : isCorner)
+            for (i = 1; i < n; i++) {
+                for (j = 1; j < m; j++) {
+                    if (field[i][j] == 'x') {
+                        for (k = 0; k < 4; k++) {
+                            dx1 = std::get<1>(direction[k]);
+                            dy1 = std::get<2>(direction[k]);
+                            adj1 = field[i + dx1][j + dy1];
+
+                            dx2 = std::get<1>(direction[(k + 1) % 4]);
+                            dy2 = std::get<2>(direction[(k + 1) % 4]);
+                            adj2 = field[i + dx2][j + dy2];
+
+                            isCorner = isCorner || (adj1 == '#' && adj2 == '#');
+                        }
+                    }
+                }
+            }
+        }
+
+        if (isCorner) return true;
+
+        return false;
+    }
+
+    std::string solve()
+    {
+        boost::unordered_set<GameMap, boost::hash<GameMap>> visited;
+        std::queue<State> stateQueue;
+        bool foundSol = false;
+
+        stateQueue.emplace(initField, playerPosX, playerPosY, "");
+        visited.insert(initField);
+
+        while(!stateQueue.empty() && !foundSol) {
+            State currState = stateQueue.front();
+            stateQueue.pop();
+
+            GameMap field = std::get<0>(currState);
+            int x = std::get<1>(currState);
+            int y = std::get<2>(currState);
+            std::string actionSeq = std::get<3>(currState);
 
 #ifdef DEBUG
-    void debug() const 
-    {
-        using std::cout;
-
-        cout << "Map:\n";
-        for (const auto& s : gameMap) {
-            cout << s << '\n';
-        }
-
-        cout << "boxPos: ";
-        for (const auto& c : boxPos) {
-            c.debug();
-            cout << ' ';
-        }
-
-        cout << "  playerPos: ";
-        playerPos.debug();
-
-        cout << " keyNode: " << keyNode->key;
-
-        cout << " hashKey: " << hashKey << '\n';
-    }
+            std::cout << "Field:\n";
+            for (auto &r : field) {
+                for (auto &c : r) std::cout << c;
+                std::cout << '\n';
+            }
+            std::cout << "PlayerPos: " << x << ' ' << y << ' ';
+            std::cout << "actionSeq: " << actionSeq << '\n';
 #endif
 
-    vector<string> gameMap;
-    vector<Coord> boxPos;
-    Coord playerPos;
-    Node* keyNode;
-    string hashKey;
-};
+            #pragma omp parallel default(none) \
+            shared(ans, actionSeq, direction, field, x, y, visited, stateQueue, foundSol)
+            {
+                int i, dx, dy;
+                char key;
+                GameMap newField;
+                bool isVisited;
 
-class Record {
-public:
-    bool contain(const State &state)
-    {
-        auto it = history.find(state.hashKey);
-        return (it != history.end());
-    }
+                #pragma omp for private(i, key, dx, dy, newField, isVisited)
+                for (i = 0; i < 4; i++) {
+                    key = std::get<0>(direction[i]);
+                    dx = std::get<1>(direction[i]);
+                    dy = std::get<2>(direction[i]);
 
-    void insert(const State &state)
-    {
-        history.insert(state.hashKey);
+                    if (!isValidStep(field, x, y, dx, dy)) continue;
+
+                    newField = field;
+
+                    makeStep(newField, x, y, dx, dy);
+
+                    #pragma omp critical
+                    {
+                        isVisited = (visited.find(newField) != visited.end());
+                        if (!isVisited) visited.insert(newField);
+                    }
+
+                    if (!isVisited) {
+                        if (isSolution(newField)) {
+                            #pragma omp critical
+                            {
+                                ans = actionSeq + key;
+                                foundSol = true;
+                            }
+                        }
+
+                        if (!isDeadlock(newField, x + dx, y + dy)) {
+                            #pragma omp critical
+                            {
+                                stateQueue.emplace(newField, x + dx, y + dy, actionSeq + key);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
     }
 
 #ifdef DEBUG
     void debug()
     {
-        std::cout << "Record: ";
-        for (const auto& s : history) {
-            std::cout << s << ' ';
-        }
-        std::cout << '\n';
-    }
-#endif
+        using std::cout;
 
-    std::unordered_set<std::string> history;
-};
-
-bool isValidMove(const State &state, const Move &m)
-{
-    Coord newPlayerPos = state.playerPos + m.dir;
-    char playerTile = state.gameMap[newPlayerPos.x][newPlayerPos.y];
-
-    if (playerTile != '#') {
-        if (playerTile == 'x' || playerTile == 'X') {
-            Coord newBoxPos = state.playerPos + 2 * m.dir;
-            char boxTile = state.gameMap[newBoxPos.x][newBoxPos.y];
-
-            return (boxTile == ' ' || boxTile == '.');
-        }
-        else
-            return true;
-    }
-    else
-        return false;
-}
-
-bool isSolution(const State& state)
-{
-    for (int i = 1; i < state.gameMap.size() - 1; i++) {
-        string s = state.gameMap[i];
-        if (s.find_first_of(".xO") != string::npos) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-string getActionSequence(const State& state)
-{
-    Node *current = state.keyNode;
-    string actionSeq;
-
-    while (current != nullptr) {
-        actionSeq.push_back(current->key);
-        current = current->prevNode;
-    }
-
-    std::reverse(actionSeq.begin(), actionSeq.end());
-
-    return actionSeq;
-}
-
-bool isDeadlock(const State& state)
-{
-    return false;
-}
-
-string findActionSequence(vector<string> &initGameMap)
-{
-    State initState = State(initGameMap);
-    Record visited;
-    queue<State> Q;
-    vector<Node*> nodes;
-    Move moves[4] = {
-        { 'W', -1, 0 }, { 'A', 0, -1 }, { 'S', 1, 0 }, { 'D', 0, 1 }
-    };
-
-#ifndef DEBUG
-    Q.emplace(initState);
-    visited.insert(initState);
-
-    while (!Q.empty()) {
-        State currState = Q.front();
-        Q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            Move m = moves[i];
-
-            if (!isValidMove(currState, m)) continue;
-
-            State newState = currState + m;
-
-            if (!visited.contain(newState)) {
-                visited.insert(newState);
-                nodes.emplace_back(newState.keyNode);
-
-                if (isSolution(newState)) {
-                    string ans = getActionSequence(newState);
-                    for (auto& n : nodes) delete n;
-                    return ans;
-                }
-
-                if (!isDeadlock(newState)) Q.emplace(newState);
+        cout << "initField:\n";
+        for (const auto &r : initField) {
+            for (const auto &c : r) {
+                cout << c;
             }
-            else
-                delete newState.keyNode;
+            cout << '\n';
         }
-    }
-#endif
 
-#ifdef DEBUG
-    Q.emplace(initState);
-    visited.insert(initState);
-
-    using std::cout;
-
-    while (!Q.empty()) {
-        State currState = Q.front();
-        Q.pop();
-
-        cout << "Current state:\n";
-        currState.debug();
+        cout << "Direction: ";
+        for (const auto &d : direction) {
+            cout << std::get<0>(d) << ',' << std::get<1>(d) << ','<< std::get<2>(d);
+            cout << "   ";
+        }
         cout << '\n';
 
+        cout << "playerPos: " << playerPosX << ',' << playerPosY << '\n';
+        cout << "isSolution: " << isSolution(initField) << '\n';
+        cout << "isDeadlock: " << isDeadlock(initField, playerPosX, playerPosY) << '\n';
+
         for (int i = 0; i < 4; i++) {
-            Move m = moves[i];
+            int dx = std::get<1>(direction[i]);
+            int dy = std::get<2>(direction[i]);
 
-            cout << "Current move: " << m.key << '\n';
-
-            if (!isValidMove(currState, m)) {
-                cout << "Invalid move: " << m.key << '\n';
-                continue;
-            }
-
-            State newState = currState + m;
-
-            cout << "New State:\n";
-            newState.debug();
-
-            if (!visited.contain(newState)) {
-                cout << "Not visited!\n";
-                visited.insert(newState);
-                nodes.emplace_back(newState.keyNode);
-
-                if (isSolution(newState)) {
-                    cout << "Solution!\n";
-                    string ans = getActionSequence(newState);
-
-                    for (auto& n : nodes) delete n;
-
-                    return ans;
+            if (isValidStep(initField, playerPosX, playerPosY, dx, dy)) {
+                GameMap field = initField;
+                makeStep(field, playerPosX, playerPosY, dx, dy);
+                cout << "newField:\n";
+                for (const auto &r : field) {
+                    for (const auto &c : r) {
+                        cout << c;
+                    }
+                    cout << '\n';
                 }
-                else cout << "Not solution!\n";
-
-                if (!isDeadlock(newState)) {
-                    cout << "Not deadlock!\n";
-                    Q.emplace(newState);
-                }
+                cout << "isSolution: " << isSolution(field) << '\n';
+                cout << "isDeadlock: " << isDeadlock(field, playerPosX, playerPosY) << '\n';
             }
-            else {
-                cout << "Visited\n";
-                delete newState.keyNode;
-            }
-            cout << "Number of States in queue: " << Q.size() << '\n';
-            cout << "\n\n\n";
         }
+
+        cout << "\n\n\n";
     }
 #endif
 
-    return "";
-}
-
-void createMapData(char* &mapfile, vector<string>& initGameMap)
-{
-    std::ifstream testcase(mapfile);
-    string line;
-
-    if (testcase.is_open()) {
-        while (std::getline(testcase, line)) {
-            initGameMap.emplace_back(line);
-        }
-        testcase.close();
-    } 
-    else 
-        std::cerr << "Unable to open file.\n";
-}
+private:
+    std::string ans;
+    GameMap initField;
+    int playerPosX, playerPosY;
+    std::vector<std::tuple<char, int, int>> direction;
+};
 
 int main(int argc, char* argv[])
 {
-    vector<string> initGameMap;
+    Game game(argv[1]);
 
-    createMapData(argv[1], initGameMap);
+    std::cout << game.solve() << '\n';
 
-    std::cout << findActionSequence(initGameMap) << '\n';
-
-    return 0;
+	return 0;
 }
