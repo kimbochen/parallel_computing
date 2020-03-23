82e120aec6be01ebe591f2d5a9db856fdeaf0560
K-mach <chentenghung@gmail.com>
2020-03-24 00:34:35 +0800

    Optimized deadlock detection.

--- hw1.cc
+++ hw1.cc
@@ -115,47 +115,22 @@ public:
         return isSol;
     }
 
-    bool isDeadlock(const GameMap &field, int x, int y)
+    bool isDeadlock(const GameMap &field, int x, int y, int dx, int dy)
     {
-        int n = field.size() - 1;
-        int m = field[0].size() - 1;
+        int boxX = x + dx, boxY = y + dy;
 
-        // A box is in a corner:
-        // ##~~ -> T
-        // ~##~ -> T
-        // ~~## -> T
-        // #~~# -> T
-        bool isCorner = false;
+        if (field[boxX][boxY] != 'x') return false;
 
-        #pragma omp parallel default(none) shared(n, m, field, isCorner)
-        {
-            int i, j, k;
-            int dx1, dy1, dx2, dy2;
-            char adj1, adj2;
+        std::string adjTiles(6, '~');
 
-            #pragma omp for private(i, j, k) reduction(|| : isCorner)
-            for (i = 1; i < n; i++) {
-                for (j = 1; j < m; j++) {
-                    if (field[i][j] == 'x') {
-                        for (k = 0; k < 4; k++) {
-                            dx1 = std::get<1>(direction[k]);
-                            dy1 = std::get<2>(direction[k]);
-                            adj1 = field[i + dx1][j + dy1];
-
-                            dx2 = std::get<1>(direction[(k + 1) % 4]);
-                            dy2 = std::get<2>(direction[(k + 1) % 4]);
-                            adj2 = field[i + dx2][j + dy2];
-
-                            isCorner = isCorner || (adj1 == '#' && adj2 == '#');
-                        }
-                    }
-                }
-            }
+        for (int i = 0; i < 4; i++) {
+            int adjX = boxX + std::get<1>(direction[i]);
+            int adjY = boxY + std::get<2>(direction[i]);
+            adjTiles[i + 1] = field[adjX][adjY];
         }
+        adjTiles[5] = adjTiles[1];
 
-        if (isCorner) return true;
-
-        return false;
+        return (adjTiles.find("##") != std::string::npos);
     }
 
     std::string solve()
@@ -167,27 +142,30 @@ public:
         stateQueue.emplace(initField, playerPosX, playerPosY, "");
         visited.insert(initField);
 
+        int iter_num = 0;
         while(!stateQueue.empty() && !foundSol) {
+            iter_num++;
             State currState = stateQueue.front();
             stateQueue.pop();
+            /*
+            #ifdef DEBUG
+                std::cout << "Field:\n";
+                for (auto &r : newField) {
+                    for (auto &c : r) std::cout << c;
+                    std::cout << '\n';
+                }
+                std::cout << "PlayerPos: " << x+dx << ' ' << y+dy << ' ';
+                std::cout << "actionSeq: " << actionSeq+key;
+            #endif
+            */
 
             GameMap field = std::get<0>(currState);
             int x = std::get<1>(currState);
             int y = std::get<2>(currState);
             std::string actionSeq = std::get<3>(currState);
 
-#ifdef DEBUG
-            std::cout << "Field:\n";
-            for (auto &r : field) {
-                for (auto &c : r) std::cout << c;
-                std::cout << '\n';
-            }
-            std::cout << "PlayerPos: " << x << ' ' << y << ' ';
-            std::cout << "actionSeq: " << actionSeq << '\n';
-#endif
-
             #pragma omp parallel default(none) \
-            shared(ans, actionSeq, direction, field, x, y, visited, stateQueue, foundSol)
+            shared(std::cout, ans, actionSeq, direction, field, x, y, visited, stateQueue, foundSol)
             {
                 int i, dx, dy;
                 char key;
@@ -221,7 +199,7 @@ public:
                             }
                         }
 
-                        if (!isDeadlock(newField, x + dx, y + dy)) {
+                        if (!isDeadlock(newField, x + dx, y + dy, dx, dy)) {
                             #pragma omp critical
                             {
                                 stateQueue.emplace(newField, x + dx, y + dy, actionSeq + key);
@@ -231,6 +209,8 @@ public:
                 }
             }
         }
+
+        std::cout << "Iter num: " << iter_num << '\n';
 
         return ans;
     }
@@ -257,7 +237,7 @@ public:
 
         cout << "playerPos: " << playerPosX << ',' << playerPosY << '\n';
         cout << "isSolution: " << isSolution(initField) << '\n';
-        cout << "isDeadlock: " << isDeadlock(initField, playerPosX, playerPosY) << '\n';
+        cout << "isDeadlock: " << isDeadlock(initField, playerPosX, playerPosY, 0, 0) << '\n';
 
         for (int i = 0; i < 4; i++) {
             int dx = std::get<1>(direction[i]);
@@ -274,7 +254,7 @@ public:
                     cout << '\n';
                 }
                 cout << "isSolution: " << isSolution(field) << '\n';
-                cout << "isDeadlock: " << isDeadlock(field, playerPosX, playerPosY) << '\n';
+                cout << "isDeadlock: " << isDeadlock(field, playerPosX, playerPosY, dx, dy) << '\n';
             }
         }
 
